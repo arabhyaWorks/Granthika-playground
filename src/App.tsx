082481d4a961fb605c2b8d80969f126f90a37f66
Arabhaya <r.arabhaya@gmail.com>
2025-01-21 22:59:01 +0530

    Granthika Playground

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@
  *
  */
 
-import {$createLinkNode} from '@lexical/link';
-import {$createListItemNode, $createListNode} from '@lexical/list';
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {$createHeadingNode, $createQuoteNode} from '@lexical/rich-text';
+import { $createLinkNode } from "@lexical/link";
+import { $createListItemNode, $createListNode } from "@lexical/list";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { $createHeadingNode, $createQuoteNode } from "@lexical/rich-text";
 import {
   $createParagraphNode,
   $createTextNode,
@@ -17,105 +17,105 @@ import {
   $isTextNode,
   DOMConversionMap,
   TextNode,
-} from 'lexical';
+} from "lexical";
 
-import {isDevPlayground} from './appSettings';
-import {FlashMessageContext} from './context/FlashMessageContext';
-import {SettingsContext, useSettings} from './context/SettingsContext';
-import {SharedHistoryContext} from './context/SharedHistoryContext';
-import {ToolbarContext} from './context/ToolbarContext';
-import Editor from './Editor';
-import logo from './images/logo.svg';
-import PlaygroundNodes from './nodes/PlaygroundNodes';
-import DocsPlugin from './plugins/DocsPlugin';
-import PasteLogPlugin from './plugins/PasteLogPlugin';
-import {TableContext} from './plugins/TablePlugin';
-import TestRecorderPlugin from './plugins/TestRecorderPlugin';
-import {parseAllowedFontSize} from './plugins/ToolbarPlugin/fontSize';
-import TypingPerfPlugin from './plugins/TypingPerfPlugin';
-import Settings from './Settings';
-import PlaygroundEditorTheme from './themes/PlaygroundEditorTheme';
-import {parseAllowedColor} from './ui/ColorPicker';
+import { isDevPlayground } from "./appSettings";
+import { FlashMessageContext } from "./context/FlashMessageContext";
+import { SettingsContext, useSettings } from "./context/SettingsContext";
+import { SharedHistoryContext } from "./context/SharedHistoryContext";
+import { ToolbarContext } from "./context/ToolbarContext";
+import Editor from "./Editor";
+import logo from "./images/logo.svg";
+import PlaygroundNodes from "./nodes/PlaygroundNodes";
+import DocsPlugin from "./plugins/DocsPlugin";
+import PasteLogPlugin from "./plugins/PasteLogPlugin";
+import { TableContext } from "./plugins/TablePlugin";
+import TestRecorderPlugin from "./plugins/TestRecorderPlugin";
+import { parseAllowedFontSize } from "./plugins/ToolbarPlugin/fontSize";
+import TypingPerfPlugin from "./plugins/TypingPerfPlugin";
+import Settings from "./Settings";
+import PlaygroundEditorTheme from "./themes/PlaygroundEditorTheme";
+import { parseAllowedColor } from "./ui/ColorPicker";
 
 console.warn(
-  'If you are profiling the playground app, please ensure you turn off the debug view. You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.',
+  "If you are profiling the playground app, please ensure you turn off the debug view. You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting."
 );
 
 function $prepopulatedRichText() {
   const root = $getRoot();
   if (root.getFirstChild() === null) {
-    const heading = $createHeadingNode('h1');
-    heading.append($createTextNode('Welcome to the playground'));
+    const heading = $createHeadingNode("h1");
+    heading.append($createTextNode("Welcome to the playground"));
     root.append(heading);
     const quote = $createQuoteNode();
     quote.append(
       $createTextNode(
         `In case you were wondering what the black box at the bottom is – it's the debug view, showing the current state of the editor. ` +
-          `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`,
-      ),
+          `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`
+      )
     );
     root.append(quote);
     const paragraph = $createParagraphNode();
     paragraph.append(
-      $createTextNode('The playground is a demo environment built with '),
-      $createTextNode('@lexical/react').toggleFormat('code'),
-      $createTextNode('.'),
-      $createTextNode(' Try typing in '),
-      $createTextNode('some text').toggleFormat('bold'),
-      $createTextNode(' with '),
-      $createTextNode('different').toggleFormat('italic'),
-      $createTextNode(' formats.'),
+      $createTextNode("The playground is a demo environment built with "),
+      $createTextNode("@lexical/react").toggleFormat("code"),
+      $createTextNode("."),
+      $createTextNode(" Try typing in "),
+      $createTextNode("some text").toggleFormat("bold"),
+      $createTextNode(" with "),
+      $createTextNode("different").toggleFormat("italic"),
+      $createTextNode(" formats.")
     );
     root.append(paragraph);
     const paragraph2 = $createParagraphNode();
     paragraph2.append(
       $createTextNode(
-        'Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!',
-      ),
+        "Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!"
+      )
     );
     root.append(paragraph2);
     const paragraph3 = $createParagraphNode();
     paragraph3.append(
-      $createTextNode(`If you'd like to find out more about Lexical, you can:`),
+      $createTextNode(`If you'd like to find out more about Lexical, you can:`)
     );
     root.append(paragraph3);
-    const list = $createListNode('bullet');
+    const list = $createListNode("bullet");
     list.append(
       $createListItemNode().append(
         $createTextNode(`Visit the `),
-        $createLinkNode('https://lexical.dev/').append(
-          $createTextNode('Lexical website'),
+        $createLinkNode("https://lexical.dev/").append(
+          $createTextNode("Lexical website")
         ),
-        $createTextNode(` for documentation and more information.`),
+        $createTextNode(` for documentation and more information.`)
       ),
       $createListItemNode().append(
         $createTextNode(`Check out the code on our `),
-        $createLinkNode('https://github.com/facebook/lexical').append(
-          $createTextNode('GitHub repository'),
+        $createLinkNode("https://github.com/facebook/lexical").append(
+          $createTextNode("GitHub repository")
         ),
-        $createTextNode(`.`),
+        $createTextNode(`.`)
       ),
       $createListItemNode().append(
         $createTextNode(`Playground code can be found `),
         $createLinkNode(
-          'https://github.com/facebook/lexical/tree/main/packages/lexical-playground',
-        ).append($createTextNode('here')),
-        $createTextNode(`.`),
+          "https://github.com/facebook/lexical/tree/main/packages/lexical-playground"
+        ).append($createTextNode("here")),
+        $createTextNode(`.`)
       ),
       $createListItemNode().append(
         $createTextNode(`Join our `),
-        $createLinkNode('https://discord.com/invite/KmG4wQnnD9').append(
-          $createTextNode('Discord Server'),
+        $createLinkNode("https://discord.com/invite/KmG4wQnnD9").append(
+          $createTextNode("Discord Server")
         ),
-        $createTextNode(` and chat with the team.`),
-      ),
+        $createTextNode(` and chat with the team.`)
+      )
     );
     root.append(list);
     const paragraph4 = $createParagraphNode();
     paragraph4.append(
       $createTextNode(
-        `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`,
-      ),
+        `Lastly, we're constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).`
+      )
     );
     root.append(paragraph4);
   }
@@ -124,17 +124,17 @@ function $prepopulatedRichText() {
 function getExtraStyles(element: HTMLElement): string {
   // Parse styles from pasted input, but only if they match exactly the
   // sort of styles that would be produced by exportDOM
-  let extraStyles = '';
+  let extraStyles = "";
   const fontSize = parseAllowedFontSize(element.style.fontSize);
   const backgroundColor = parseAllowedColor(element.style.backgroundColor);
   const color = parseAllowedColor(element.style.color);
-  if (fontSize !== '' && fontSize !== '15px') {
+  if (fontSize !== "" && fontSize !== "15px") {
     extraStyles += `font-size: ${fontSize};`;
   }
-  if (backgroundColor !== '' && backgroundColor !== 'rgb(255, 255, 255)') {
+  if (backgroundColor !== "" && backgroundColor !== "rgb(255, 255, 255)") {
     extraStyles += `background-color: ${backgroundColor};`;
   }
-  if (color !== '' && color !== 'rgb(0, 0, 0)') {
+  if (color !== "" && color !== "rgb(0, 0, 0)") {
     extraStyles += `color: ${color};`;
   }
   return extraStyles;
@@ -165,7 +165,7 @@ function buildImportMap(): DOMConversionMap {
           }
           const extraStyles = getExtraStyles(element);
           if (extraStyles) {
-            const {forChild} = output;
+            const { forChild } = output;
             return {
               ...output,
               forChild: (child, parent) => {
@@ -188,17 +188,17 @@ function buildImportMap(): DOMConversionMap {
 
 function App(): JSX.Element {
   const {
-    settings: {isCollab, emptyEditor, measureTypingPerf},
+    settings: { isCollab, emptyEditor, measureTypingPerf },
   } = useSettings();
 
   const initialConfig = {
     editorState: isCollab
       ? null
       : emptyEditor
-      ? undefined
-      : $prepopulatedRichText,
-    html: {import: buildImportMap()},
-    namespace: 'Playground',
+        ? undefined
+        : $prepopulatedRichText,
+    html: { import: buildImportMap() },
+    namespace: "Playground",
     nodes: [...PlaygroundNodes],
     onError: (error: Error) => {
       throw error;
@@ -212,9 +212,7 @@ function App(): JSX.Element {
         <TableContext>
           <ToolbarContext>
             <header>
-              <a href="https://lexical.dev" target="_blank" rel="noreferrer">
-                <img src={logo} alt="Lexical Logo" />
-              </a>
+              <h1>Granthika Playground</h1>
             </header>
             <div className="editor-shell">
               <Editor />
@@ -241,27 +239,29 @@ export default function PlaygroundApp(): JSX.Element {
       <a
         href="https://github.com/facebook/lexical/tree/main/packages/lexical-playground"
         className="github-corner"
-        aria-label="View source on GitHub">
+        aria-label="View source on GitHub"
+      >
         <svg
           width="80"
           height="80"
           viewBox="0 0 250 250"
           style={{
-            border: '0',
-            color: '#eee',
-            fill: '#222',
-            left: '0',
-            position: 'absolute',
-            top: '0',
-            transform: 'scale(-1,1)',
+            border: "0",
+            color: "#eee",
+            fill: "#222",
+            left: "0",
+            position: "absolute",
+            top: "0",
+            transform: "scale(-1,1)",
           }}
-          aria-hidden="true">
+          aria-hidden="true"
+        >
           <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
           <path
             d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
             fill="currentColor"
             style={{
-              transformOrigin: '130px 106px',
+              transformOrigin: "130px 106px",
             }}
             className="octo-arm"
           />
